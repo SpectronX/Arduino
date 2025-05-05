65501b8cdba3bbba14bf4f4bbbf9e9fd1cc73afa
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-05-05 14:46:57 +0000

    Update Code.ino

--- Home Automation/Code.ino
+++ Home Automation/Code.ino
@@ -1,1 +1,25 @@
+int pir = 12;
+int buzzer = 7;
 
+void setup()
+{
+  pinMode(pir, INPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int pirState = digitalRead(pir);
+  
+  switch (pirState){
+    case HIGH:{
+      digitalWrite(buzzer, HIGH);
+      break;
+    }
+    case LOW:{
+      digitalWrite(buzzer, LOW);
+      break;
+    }
+  }
+}
