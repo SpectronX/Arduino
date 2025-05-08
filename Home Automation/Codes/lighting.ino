a76cde34e27363569e0c191d05165124a5a11362
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-05-08 14:31:37 +0000

    Update lighting.ino

--- Home Automation/Codes/lighting.ino
+++ Home Automation/Codes/lighting.ino
@@ -1,1 +1,22 @@
+// C++ code
+//
+int prPin = A0;
+int bulb = 2;
+int threshold = 500;
 
+void setup()
+{
+  pinMode(bulb, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(analogRead(prPin) > threshold ){
+    Serial.println("The sun is out");
+    digitalWrite(bulb, LOW);
+  } else {
+    Serial.println("Darkness");
+    digitalWrite(bulb, HIGH);
+  }
+}
