d768e4c5d70be53f680c4308d5eacef2cab9ef15
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-05-09 13:48:07 +0000

    Update fan.ino

--- Home Automation/Codes/fan.ino
+++ Home Automation/Codes/fan.ino
@@ -1,1 +1,30 @@
+int tempPin = A0;
+int fanPin = 6;
 
+void setup(){
+  pinMode(tempPin, INPUT);
+  pinMode(fanPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int sensorValue = analogRead(tempPin);
+
+  // Getting the temperature value in Celsius
+  float voltage = (5.0/1024.0) * sensorValue;
+  float tempC = (100 * voltage) - 50;
+
+  Serial.print("Temperature: ");
+  Serial.println(tempC);
+  
+  if (tempC > 35) {
+    analogWrite(fanPin, 255);
+  } else if (tempC > 30) {
+    analogWrite(fanPin, 180);
+  } else if (tempC > 25) {
+    analogWrite(fanPin, 100);
+  } else {
+    analogWrite(fanPin, 0);
+  }
+
+}
